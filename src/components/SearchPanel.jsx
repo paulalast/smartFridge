48985e46a8a6942f0852f76322aef961a7e30026
Paulina <paulalastowiecka@gmail.com>
2023-08-10 19:50:09 +0200

    api delay

--- src/components/SearchPanel.jsx
+++ src/components/SearchPanel.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react"
-import { useQuery } from "react-query"
+import React, { useState } from "react"
 
 const AddBtn = () => {
 	function addProductToTheFridge() {
@@ -10,7 +9,7 @@ const AddBtn = () => {
 			className='w-9 h-9 place-self-center items-center mx-4'
 			onClick={addProductToTheFridge}
 		>
-			<img className='w-full h-full object-fit ' src='add.svg' alt='add' />
+			<img className='w-full h-full object-fit' src='add.svg' alt='add' />
 		</button>
 	)
 }
@@ -18,84 +17,68 @@ const AddBtn = () => {
 function SearchPanel() {
 	const [searchProduct, setSearchProduct] = useState("")
 	const [searchResults, setSearchResults] = useState([])
-	const [allProducts, setAllProducts] = useState([])
+	const [timer, setTimer] = useState(null)
 
-	const fetchAllProducts = async () => {
-		const API_KEY = process.env.REACT_APP_SPOONACULAR_API_KEY
-		const response = await fetch(
-			`https://api.spoonacular.com/food/ingredients/search?apiKey=${API_KEY}`
+	function fetchAllProducts(query) {
+		const API_KEY = "74c9df41a61f44b49363d25085ceeade"
+
+		fetch(
+			`https://api.spoonacular.com/food/ingredients/search?query=${query}&apiKey=${API_KEY}`
 		)
-		console.log("start API")
-		const data = await response.json()
-		return data.results
+			.then(response => response.json())
+			.then(data => {
+				console.log("API response:", data)
+
+				setSearchResults(data.results)
+			})
+			.catch(error => {
+				console.error("Error fetching data:", error)
+			})
 	}
 
-	const { data, isLoading } = useQuery(["allProducts"], fetchAllProducts)
-
-	useEffect(() => {
-		if (!isLoading && data) {
-			setAllProducts(data)
-		}
-	}, [isLoading, data])
-
-	useEffect(() => {
-		let timerId
-
-		if (searchProduct.length >= 3) {
-			timerId = setTimeout(() => {
-				const filteredProducts = allProducts.filter(product =>
-					product.name.toLowerCase().includes(searchProduct.toLowerCase())
-				)
-				setSearchResults(filteredProducts)
-			}, 1000)
-		} else {
-			setSearchResults([])
-		}
-
-		return () => clearTimeout(timerId)
-	}, [searchProduct, allProducts])
-
-	const showList = searchProduct.length > 0
 	const handleSearch = e => {
 		setSearchProduct(e.target.value)
+		if (timer) clearTimeout(timer)
+		const newTimer = setTimeout(() => {
+			fetchAllProducts(e.target.value)
+		}, 2000)
+		setTimer(newTimer)
 	}
 
 	return (
-		<div className='flex flex-col w-full justify-center bg-pink-100 items-center overflow-y-auto '>
+		<div className='flex flex-col w-full justify-center bg-pink-100 items-center overflow-y-auto'>
 			<input
 				type='text'
 				placeholder='Search for a product...'
-				className='search-input w-5/6 h-20 px-4 py-2 text-2xl font-textFont  border border-mainTransparent '
+				className='search-input w-5/6 h-20 px-4 py-2 text-2xl font-textFont border border-mainTransparent'
 				value={searchProduct}
 				onChange={handleSearch}
 			/>
-			{showList && (
-				<ul className='mt-4 w-5/6 my-2 pl-6 font-textFont border py-1 max-h-[250] overflow-y-auto  '>
-					{searchResults.map(food => (
-						<div className='flex h-fit max-h-full  ' key={food.id}>
-							<li className='flex w-full bg-yellow-100 flex-row p-2 text-2xl text-left border-b'>
-								{food.name}
-							</li>
-							<input
-								type='number'
-								id='quantity'
-								inputMode='numeric'
-								pattern='[0-9]*'
-								placeholder='1'
-								min='0'
-								step='1'
-								className='w-16 mr-6  bg-blue-100 m-0 text-center '
-							/>
-							<select id='unit ' className='w-fit '>
-								<option value='pcs'>pieces</option>
-								<option value='l'>liters</option>
-								<option value='g'>grams</option>
-							</select>
-							<AddBtn />
-						</div>
-					))}
-				</ul>
-			)}
+			<ul className='mt-4 w-5/6 my-2 pl-6 font-textFont border py-1 max-h-[250] overflow-y-auto'>
+				{searchResults.map(food => (
+					<div className='flex h-fit max-h-full' key={food.id}>
+						<li className='flex w-full bg-yellow-100 flex-row p-2 text-2xl text-left border-b'>
+							{food.name}
+						</li>
+						<input
+							type='number'
+							id='quantity'
+							inputMode='numeric'
+							pattern='[0-9]*'
+							placeholder='1'
+							min='0'
+							step='1'
+							className='w-16 mr-6 bg-blue-100 m-0 text-center'
+						/>
+						<select id='unit' className='w-fit'>
+							<option value='pcs'>pieces</option>
+							<option value='l'>liters</option>
+							<option value='g'>grams</option>
+						</select>
+						<AddBtn />
+					</div>
+				))}
+			</ul>
 		</div>
 	)
 }
